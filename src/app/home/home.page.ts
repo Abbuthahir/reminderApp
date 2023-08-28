4dce0c14e43c78459408422e25598f89a5fd4dc3
Abbuthahir <abbuthahir04@gmail.com>
2023-08-28 18:03:26 +0530

    28/08/2023

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  items = [
+    { name: 'Today', icon:'calendar-number-outline', color: 'primary', selected: false  },
+    { name: 'Scheduled', icon:'calendar', color: 'danger',  selected: false },
+    { name: 'All', icon: 'file-tray', color: 'dark',  selected: false },
+    { name: 'Flagged', icon: 'flag-outline', color: 'warning',  selected: false },
+    { name: 'Completed', icon:'checkmark', color:'gray',  selected: false } ];
+  editing: boolean = false;
+  selectedItems: any[] = [];
+ 
 
-  constructor() {}
+
+  constructor() { }
+
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+    ev.detail.complete();
+  }
+  toggleSelection(index: number) {
+    this.items[index].selected = !this.items[index].selected;
+  }
+  toggleEditMode() {
+    if (this.editing) {
+      // If exiting edit mode, store selected items
+      this.selectedItems = this.items.filter(item => item.selected);
+
+    }
+    this.editing = !this.editing;
+    console.log('selected', this.selectedItems);
+  }
 
 }
