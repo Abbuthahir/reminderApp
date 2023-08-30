0370e9d099b55a3d85ce82fc9977f2a6eeaf30a5
Abbuthahir <abbuthahir04@gmail.com>
2023-08-30 17:39:59 +0530

    30/8/2023

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,32 +1,48 @@
 import { Component } from '@angular/core';
 import { ItemReorderEventDetail } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
   items = [
     { name: 'Today', icon: 'calendar-number-outline', color: 'primary', selected: true },
     { name: 'Scheduled', icon: 'calendar', color: 'danger', selected: true },
-    { name: 'All', icon: 'file-tray', color: 'dark', selected: true },
+    { name: 'All', icon: 'file-tray', color: 'dark', selected: false },
     { name: 'Flagged', icon: 'flag-outline', color: 'warning', selected: true },
-    { name: 'Completed', icon: 'checkmark', color: 'medium', selected: false },
+    { name: 'Completed', icon: 'checkmark', color: 'medium', selected: true },
     { name: 'Assigned', icon: 'person', color: 'success', selected: false }];
   editing: boolean = false;
   selectedItems: any[] = this.items.filter(item => item.selected);
   activeSegment = 'newlist';
+  selectedColor: string = '#1E90FF';
+  selectedIcon: string = 'list';
+  selectedListType: string = 'standard';
+  selectedListTypeIcon: string = 'list';
   colors: string[] = [
     'red', 'orange', '#FFC300', '#32CD32', '#89CFF0', '#1E90FF',
-    '#FFC300', '#FF5733', '#B65FCF', '#C70039', '#900C3F', '#581845', '#FF5733',
+    '#7C37CC', '#FF5733', '#B65FCF', '#BF946F', '#777B7E', '#E4C7B7'
   ];
   colorChunks: string[][] = [];
 
+  listIcons: any = ['list', 'bookmark', 'pin', 'gift', 'school', 'document', 'walk',
+    'thermometer', 'book', 'card', 'barbell', 'restaurant', 'wine', 'git-network-outline', 'home', 'business',
+    'tv-outline', 'musical-notes', 'game-controller', 'headset', 'leaf',];
+
   constructor() {
     this.colorChunks = this.chunkArray(this.colors, 7);
-   }
-   private chunkArray(array: any[], size: number): any[][] {
+  }
+  selectColor(color: string) {
+    this.selectedColor = color;
+  }
+  selectIcon(icon: string) {
+    this.selectedIcon = icon;
+  }
+  private chunkArray(array: any[], size: number): any[][] {
     const result = [];
     for (let i = 0; i < array.length; i += size) {
       result.push(array.slice(i, i + size));
@@ -50,5 +66,21 @@ export class HomePage {
   }
   async canDismiss(data?: any, role?: string) {
     return role !== 'gesture';
+  }
+  updateListType() {
+    switch (this.selectedListType) {
+      case 'standard':
+        this.selectedListTypeIcon = 'list';
+        break;
+      case 'shopping':
+        this.selectedListTypeIcon = 'cart';
+        break;
+      case 'smartlist':
+        this.selectedListTypeIcon = 'filter-circle-outline';
+        break;
+      default:
+        this.selectedListTypeIcon = 'list';
+        break;
+    }
   }
 }
